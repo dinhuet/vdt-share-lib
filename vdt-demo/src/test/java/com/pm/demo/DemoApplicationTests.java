5017ef465826398cc44f59e37f2e80f096dc2a72
Dinh Tran <23020043@vnu.edu.vn>
2026-06-13 18:13:00 +0700

    dinh: demo test

--- vdt-demo/src/test/java/com/pm/demo/DemoApplicationTests.java
+++ vdt-demo/src/test/java/com/pm/demo/DemoApplicationTests.java
@@ -1,7 +1,19 @@
 package com.pm.demo;
 
+import com.pm.sharedlib.runtime.SecuritySettingsStore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -10,4 +22,12 @@ class DemoApplicationTests {
     void contextLoads() {
     }
 
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        @Primary
+        StringRedisTemplate stringRedisTemplate() {
+            return mock(StringRedisTemplate.class);
+        }
+    }
 }
